fe37fc7aec171877ae08fc0bfc47f3c899680d99
idansof <idansof@89a9692b-d3f7-0310-8dc6-c76c65cf9e32>
2005-05-28 16:03:34 +0000

    r546@Stallman:  idan | 2005-05-28 15:55:44 +0300  Enhance stationInfoLauncher - process options to enable/disable servlet and update services via getopt

--- stationInfo/trunk/src/org/idanso/stationinfo/StationInfoLauncher.java
+++ stationInfo/trunk/src/org/idanso/stationinfo/StationInfoLauncher.java
@@ -3,6 +3,10 @@
  */
 package org.idanso.stationinfo;
 
+import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
+
+import org.idanso.service.Service;
 import org.idanso.service.ServiceException;
 import org.idanso.service.ServiceManager;
 import org.idanso.stationinfo.fetch.StationUpdateService;
@@ -16,21 +20,62 @@ import org.idanso.stationinfo.servlet.ServletService;
  */
 public class StationInfoLauncher {
 
+	private static String updateServiceClassName="org.idanso.stationinfo.fetch.StationUpdateService";
+	private static String servletServiceClassName="org.idanso.stationinfo.servlet.ServletService";
 	
 	/**
 	 * @param args
 	 * @throws ServiceException 
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public static void main(String[] args) throws ServiceException {
+	public static void main(String[] args) throws ServiceException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		LongOpt[] options=new LongOpt[3];
+		options[0]=new LongOpt("help",LongOpt.NO_ARGUMENT,null,'h');
+		options[1]=new LongOpt("enable-jetty",LongOpt.NO_ARGUMENT,null,'s');
+		options[2]=new LongOpt("disable-update",LongOpt.NO_ARGUMENT,null,'u');
+		Getopt opt=new Getopt(StationInfoLauncher.class.getName(),args,"",options);
+		int c;
+		boolean useServlet=false;
+		boolean useUpdate=true;
+		while( (c=opt.getopt())!=-1)
+		{
+			switch(c)
+			{
+			case 'h':
+				printHelp();
+				System.exit(0);
+				break;
+			case 's':
+				useServlet=true;
+				break;
+			case 'u':
+				useUpdate=false;
+				break;
+			}
+			
+		}		
 		ServiceManager manager;
-		StationUpdateService updateService;
-		ServletService servletService;
 		manager=ServiceManager.getInstance();
-		updateService=new StationUpdateService();
-		servletService=new ServletService();
-		manager.addService(updateService);
-		manager.addService(servletService);
+		if(useUpdate)
+		{
+			// Launch update service;
+			manager.addService((Service) Class.forName(updateServiceClassName).newInstance());
+		}
+		if(useServlet)
+		{
+			// Launch servlet service;
+			manager.addService((Service) Class.forName(servletServiceClassName).newInstance());
+		}		
 		manager.startAll();		
+	}
+
+	private static void printHelp() {
+		System.out.println("Options for launcher:");
+		System.out.println("--enable-jetty Starts weather servlet using jetty");
+		System.out.println("--disable-update Do not launch update service");
+		
 	}
 
 }
